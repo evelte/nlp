cd1042c0a53c232907f1894660c98b2451113499
elenavelte <elena@optylon.com>
2017-05-16 10:04:43 +0100

    fix: get only distinct reviews

--- import_data.py
+++ import_data.py
@@ -6,7 +6,7 @@ import shelve
 db = DataBase(fileName=None, onlyRemote=True, onlyLocal=False)
 
 # get relevant review data
-query = '''select id, listing_id, created_at, role, comments, response
+query = '''select distinct on (review_id) review_id, listing_id, created_at, role, comments, response
         from airbnb_listing_review'''
 results = db.executeQuery(db.psqlDB, query)
 
@@ -20,5 +20,5 @@ print(len(df))
 
 # store dataframe into shelve
 with shelve.open('df_reviews') as db:
-    # db['v1'] = df
-    db['sample_5000'] = df.sample(n=5000)  # get random test sample
+    db['v1'] = df
+    # db['sample_5000'] = df.sample(n=5000)  # get random test sample
