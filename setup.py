80f0170674e66b34774ea5f8b17b36a9acf3012b
elenavelte <elena@optylon.com>
2017-06-21 17:07:26 +0100

    add project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nlp_new',
+      version=version,
+      url='http://nlp_new.org',
+      description='',
+      author='',
+      license='',
+      packages=[],
+      zip_safe=False,
+      install_requires=[])
